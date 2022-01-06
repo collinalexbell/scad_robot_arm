34be55ef636352fa4e0f7744373d93bf0a4075be
kuberlog <collinalexbell@gmail.com>
2022-01-06 19:47:10 +0000

    Simulation has both environment and robot!!!

--- Simulation.py
+++ Simulation.py
@@ -1,11 +1,31 @@
 from direct.showbase.ShowBase import ShowBase
+import time
+
 
 
 class MyApp(ShowBase):
 
+
     def __init__(self):
+
         ShowBase.__init__(self)
 
 
+        # Load the environment model.
+
+        self.scene = self.loader.loadModel("models/environment")
+        self.robot = self.loader.loadModel("./onix_bot.stl")
+
+        # Reparent the model to render.
+
+        self.robot.reparentTo(self.scene)
+        self.scene.reparentTo(self.render)
+
+        # Apply scale and position transforms on the model.
+
+        self.scene.setScale(0.25, 0.25, 0.25)
+
+
 app = MyApp()
+
 app.run()
